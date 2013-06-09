e5aa9f36fb085e66cf39c21c81b2ae3422b97054
Syd Polk <syd@rocksys.com>
2013-06-08 21:04:43 -0500

    New template project for Mac version.

---
+++ creatures-macTests/creatures_macTests.h
@@ -0,0 +1,13 @@
+//
+//  creatures_macTests.h
+//  creatures-macTests
+//
+//  Created by Syd Polk on 6/8/13.
+//  Copyright (c) 2013 Bone Jarring Games and Software. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface creatures_macTests : SenTestCase
+
+@end
---
+++ creatures-macTests/creatures_macTests.m
@@ -0,0 +1,32 @@
+//
+//  creatures_macTests.m
+//  creatures-macTests
+//
+//  Created by Syd Polk on 6/8/13.
+//  Copyright (c) 2013 Bone Jarring Games and Software. All rights reserved.
+//
+
+#import "creatures_macTests.h"
+
+@implementation creatures_macTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in creatures-macTests");
+}
+
+@end
