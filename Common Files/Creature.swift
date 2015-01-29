05f27f212f956d2ea1152381805d3cf4a2f6e3e2
Syd Polk <sydpolk@gmail.com>
2015-01-28 21:22:16 -0800

    Added a subclass of NSManagedClass.

--- Common Files/Creature.swift
+++ Common Files/Creature.swift
@@ -13,6 +13,6 @@ class Creature: NSManagedObject {
     // When this is hooked up to the server, the server will have to provide a serial (or GUID)
     // for this object.
     let serial = Prefs.getNewID()
-    var name = ""
+    @NSManaged var name : NSString
 
 }
