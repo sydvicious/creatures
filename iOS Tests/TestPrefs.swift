5c3e8c8e34f54c59bffcba57cdc2d851c51c3ffc
Syd Polk <sydpolk@gmail.com>
2015-06-12 07:39:10 -0700

    Added first test. NSFetchedResultsController does not exist on Mac; we'll have to come up with something else. See http://www.raywenderlich.com/21752/how-to-use-cocoa-bindings-and-core-data-in-a-mac-app.

---
+++ iOS Tests/TestPrefs.swift
@@ -0,0 +1,32 @@
+//
+//  TestPrefs.swift
+//  Characters
+//
+//  Created by Syd Polk on 6/12/15.
+//
+//
+
+import XCTest
+import iOS
+
+class TestPrefs: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testExample() {
+        let uuid1 = Prefs.getNewID()
+        XCTAssertNotNil(uuid1, "TestPrefs: uuid1 returned nil!")
+        let uuid2 = Prefs.getNewID()
+        XCTAssertNotNil(uuid2, "TestPrefs: uuid2 returned nil!")
+        XCTAssertNotEqual(uuid1, uuid2, "TestPrefs: uuid1 and uuid2 are equal.")
+    }
+
+}
