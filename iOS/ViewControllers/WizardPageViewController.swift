f3fa3643e32f07ea2874acdf8387109032efca18
Syd Polk <sydpolk@gmail.com>
2016-11-10 22:39:53 -0600

    Reorganizing xcode project. Starting to work with the character creation wizard.

---
+++ iOS/ViewControllers/WizardPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WizardPageViewController.swift
+//  Characters
+//
+//  Created by Syd Polk on 11/10/16.
+//
+//
+
+import UIKit
+
+class WizardPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
