3a20f52c599ad17afce1c9cb76471b07731d3d91
Syd Polk <sydpolk@gmail.com>
2017-06-07 16:12:12 -0700

    More command line fixes.

--- bj_character/commands.swift
+++ bj_character/commands.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+var creaturesController: CreaturesController?
+
 enum Commands: Hashable {
     case database_open
     case database_create
@@ -20,27 +22,33 @@ enum Commands: Hashable {
     case quit
 }
 
-let single_dash_options: [String:Commands] = [
-    "o": .database_open,
-    "l": .list_characters,
-    "d": .display_character,
-    "c": .create_character,
-    "e": .edit_character,
-    "x": .delete_character,
-    "h": .help,
-    "q": .quit
-]
-
-let whole_word_options: [String:Commands] = [
-    "open": .database_open,
-    "list": .list_characters,
-    "display": .display_character,
-    "create": .create_character,
-    "edit": .edit_character,
-    "delete": .delete_character,
-    "help": .help,
-    "qquit": .quit
-
+let option_strings: [String:Commands] = [
+    "-o": .database_open,
+    "--open": .database_open,
+    "6": .database_open,
+    "-l": .list_characters,
+    "--list": .list_characters,
+    "1": .list_characters,
+    "-d": .display_character,
+    "--display": .display_character,
+    "2": .display_character,
+    "-c": .create_character,
+    "--create": .create_character,
+    "3": .create_character,
+    "-e": .edit_character,
+    "--edit": .edit_character,
+    "4": .edit_character,
+    "-x": .delete_character,
+    "--delete": .delete_character,
+    "5": .delete_character,
+    "-h": .help,
+    "--help": .help,
+    "-?": .help,
+    "-q": .quit,
+    "--exit": .quit,
+    "--quit": .quit,
+    "--bye": .quit,
+    "0": .quit
 ]
 
 let help_text: [Commands:String] = [
@@ -54,14 +62,67 @@ let help_text: [Commands:String] = [
     .quit: "quit the app after saving the database"
 ]
 
-class CommandProcessor {
+func database_open() -> Bool {
+
+    creaturesController = CreaturesController.sharedCreaturesController(CharactersContext(false))
+    return true
+}
+
+func list_characters() -> Bool {
+    return true
+}
+
+func display_character() -> Bool {
+    return true
+}
+
+func create_character() -> Bool {
+    return true
+}
+
+func edit_character() -> Bool {
+    return true
+}
+
+func delete_character() -> Bool {
+    return true
+}
+
+func help() -> Bool {
+    return true
+}
+
+func quit() -> Bool {
+    return false
+}
+
+// Generic commands
+
+let command_map : [Commands:()->Bool] = [
+    .database_open : database_open,
+    .list_characters : list_characters,
+    .display_character : display_character,
+    .create_character : create_character,
+    .edit_character : edit_character,
+    .delete_character : delete_character,
+    .help : help,
+    .quit: quit
+]
+
+func usage() {
+    let executableName = (CommandLine.arguments[0] as NSString).lastPathComponent
     
-    class func usage() {
-        let executableName = (CommandLine.arguments[0] as NSString).lastPathComponent
-        
-        print("usage:")
-        print("\(executableName) (no option) - interactive mode")
-        print("\(executableName) -h - display help")
-        print("\(executableName) -i - enter interactive mode after other options processed")
+    print("usage:")
+    print("\(executableName) (no option) - interactive mode")
+    print("\(executableName) -h - display help")
+    print("\(executableName) -i - enter interactive mode after other options processed")
+}
+
+func process_command(command_string: String) -> Bool {
+    if let command = option_strings[command_string] {
+        return command_map[command]!()
+    } else {
+        print("Unknown option " + command_string)
     }
+    return true
 }
--- bj_character/main.swift
+++ bj_character/main.swift
@@ -8,27 +8,32 @@
 
 import Foundation
 
-let batcher = Batch()
-batcher.batch()
+let arguments = CommandLine.arguments
 
+if arguments.count > 1 {
+    let args = Array(arguments[1..<arguments.count])
+    for arg in args {
+        _ = process_command(command_string: arg)
+    }
+} else {
+    print("Welcome to the Bone Jarring Creature Command Line Utility.")
+    print("1. List existing characters")
+    print("2. Display character")
+    print("3. Create character")
+    print("4. Edit character")
+    print("5. Delete character")
+    print("6. Open character database")
+    print("0. Quit")
 
-print("Welcome to the Bone Jarring Creature Command Line Utility.")
+    print("")
 
-let creaturesController = CreaturesController.sharedCreaturesController(CharactersContext(false))
+    var executing = true;
 
+    while (executing) {
+        print("Command: ", terminator:"")
+        if let command = readLine() {
+            executing = process_command(command_string: command)
+        }
+    }
 
-print("1. List existing characters")
-print("2. Display character")
-print("3. Create character")
-print("4. Edit character")
-print("5. Delete character")
-print("6. Quit")
-print("Command: ", terminator:"")
-
-var raw_input : String?
-var process_commands = true;
-
-while (process_commands) {
-    
 }
-
