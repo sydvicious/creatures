907350717fcc721b639f2134c61551c82f49e485
Syd Polk <spolk@bonejarring.com>
2022-06-26 23:06:10 -0500

    Remove main storyboard; start over with SwiftUI.

---
+++ iOS/Views/MainNavigation.swift
@@ -0,0 +1,20 @@
+//
+//  MainNavigation.swift
+//  iOS
+//
+//  Created by Syd Polk on 6/26/22.
+//
+
+import SwiftUI
+
+struct MainNavigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainNavigation_Previews: PreviewProvider {
+    static var previews: some View {
+        MainNavigation()
+    }
+}
