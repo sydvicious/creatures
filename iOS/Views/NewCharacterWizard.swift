771702218a84ecf051bdb70e8fa053f9d12f8f30
Syd Polk <spolk@bonejarring.com>
2022-07-10 13:54:33 -0500

    Add name to character wizard

--- iOS/Views/NewCharacterWizard.swift
+++ iOS/Views/NewCharacterWizard.swift
@@ -17,17 +17,76 @@ struct NewCharacterWizard: View {
     @State private var protoData = ProtoCharacter()
 
     @State private var rolls: [Rolls4d6]? = nil
+    @State private var doneDisabled = true
+    @State private var nameValid = false
+    @State private var abilitiesValid = false
     
     var body: some View {
         VStack {
             Text("New Character Wizard")
             Spacer()
-            Button("Done", action: done)
-        }.onAppear(perform: {
-            protoData = ProtoCharacter.dummyProtoData()
-            
-            print("TODO: Replace dummy characters - protoData.name")
-        })
+            Form {
+                Text("Please provide a name for your new character:")
+                TextField("<NAME>", text: $protoData.name)
+                    .onSubmit {
+                        validateName()
+                    }
+                    .onAppear {
+                        validateName()
+                    }
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .border(.secondary)
+            }
+            HStack {
+                Button("Cancel", action: cancel)
+                Button("Done", action: done)
+                    .disabled(doneDisabled)
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+    
+    private func validateName() {
+        let trimmedName = protoData.name.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        nameValid = trimmedName != ""
+        updateDoneButton()
+    }
+    
+    private func validateAbilities() {
+        guard let strength = protoData.abilities[.Strength], strength > 0 else {
+            abilitiesValid = false
+            return
+        }
+        guard let dexterity = protoData.abilities[.Dexterity], dexterity > 0 else {
+            abilitiesValid = false
+            return
+        }
+        guard let constitution = protoData.abilities[.Constitution], constitution > 0 else {
+            abilitiesValid = false
+            return
+        }
+        guard let intelligence = protoData.abilities[.Intelligence], intelligence > 0 else {
+            abilitiesValid = false
+            return
+        }
+        guard let wisdom = protoData.abilities[.Wisdom], wisdom > 0 else {
+            abilitiesValid = false
+            return
+        }
+        guard let charisma = protoData.abilities[.Charisma], charisma > 0 else {
+            abilitiesValid = false
+            return
+        }
+        abilitiesValid = true
+    }
+    
+    private func updateDoneButton() {
+        doneDisabled = !(nameValid && abilitiesValid)
+    }
+    
+    private func cancel() {
+        newCharacterWizardShowing = false
     }
     
     private func done() {
